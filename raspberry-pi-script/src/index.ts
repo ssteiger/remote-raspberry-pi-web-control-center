4a86b25f3947de0c05f476dc90e0a7d3c9778cd6
Sören Steiger <soren1@gmx.net>
2025-05-23 00:17:33 +0200

    Update example script

--- raspberry-pi-script/src/index.ts
+++ raspberry-pi-script/src/index.ts
@@ -1,5 +1,32 @@
-import { Gpio } from 'onoff'
-const led = new Gpio(17, 'out')
-const button = new Gpio(4, 'in', 'both')
+const Gpio = require('onoff').Gpio // Constructor function for Gpio objects.
 
-button.watch((err, value) => led.writeSync(value))
+console.log('Starting application...')
+console.log('Initializing GPIO...')
+const led = new Gpio(6, 'out')
+
+console.log('LED on GPIO 6 (output)')
+
+// Function to toggle LED state
+function toggleLED() {
+  led.read((err, value) => {
+    if (err) {
+      console.error('Error reading LED state:', err)
+      return
+    }
+    led.write(value === 0 ? 1 : 0, (err) => {
+      if (err) {
+        console.error('Error writing to LED:', err)
+      }
+    })
+  })
+}
+
+// Start blinking every 500ms
+const blinkInterval = setInterval(toggleLED, 500)
+
+process.on('SIGINT', () => {
+  console.log('Cleaning up GPIO...')
+  clearInterval(blinkInterval)
+  led.unexport()
+  process.exit()
+})
